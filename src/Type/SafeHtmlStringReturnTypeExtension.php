c81b9be594d9a300c78b7801be7ae37a8aba55fd
Satoshi Nishimura <nishim314@gmail.com>
2025-02-07 14:38:32 +0900

    bugfix of ReturnTypeExtension

--- src/Type/SafeHtmlStringReturnTypeExtension.php
+++ src/Type/SafeHtmlStringReturnTypeExtension.php
@@ -4,13 +4,20 @@ declare(strict_types = 1);
 
 namespace Nish\PHPStan\Type;
 
+use Nish\PHPStan\Type\Accessory\AccessorySafeHtmlStringType;
+use PHPStan\Type\IntersectionType;
+use PHPStan\Type\StringType;
+
 class SafeHtmlStringReturnTypeExtension extends DynamicReturnTypeExtension
 {
 
 	/** @param string|array<string> $func */
 	public function __construct(string|array $func)
 	{
-		parent::__construct($func, new Accessory\AccessorySafeStringType());
+		parent::__construct($func, new IntersectionType([
+			new StringType(),
+			new AccessorySafeHtmlStringType(),
+		]));
 	}
 
 }
